31b5dc347acddab12f213d85146da26e1981be7b
Eveliina work <eveliina.kallioniemi@gmail.com>
2017-05-06 22:37:22 +0200

    new habitat types added, updated csv

--- script/dataCleaning/Creating_presence_absence_file.R
+++ script/dataCleaning/Creating_presence_absence_file.R
@@ -16,8 +16,10 @@ library(dismo)
 
 data_swd <-read.table("C:/Users/kvieka/Documents/GitHub/birdDist/data/birdDist/allbird_withbackground.csv", header = TRUE, sep = ",")
 str(data_swd)
+
 bg<-subset(data_swd, species == "background_HOWR")
 str(bg)
+count(bg$landuse)
 locs <- bg[,c("longitude", "latitude")]
 head(locs)  # get site mid point coordinates
 
@@ -94,11 +96,11 @@ sum(lewo$LEWO)
 #locs_bbwo  
 locs_bbwo <-SpatialPoints(locs_bbwo)
 bbwo  <- as.data.frame(sp::over(poly, locs_bbwo))
-colnames(bbwo)<- "BBMW"
-bbwo$BBMW <- as.numeric(bbwo$BBMW)
-bbwo$BBMW<- ifelse(is.na(bbwo$BBMW), 0, 1) # NA's as 0 all other as presence=1
-bbwo$BBMW[1:20]
-sum(bbwo$BBMW)
+colnames(bbwo)<- "BBWO"
+bbwo$BBWO <- as.numeric(bbwo$BBWO)
+bbwo$BBWO<- ifelse(is.na(bbwo$BBWO), 0, 1) # NA's as 0 all other as presence=1
+bbwo$BBWO[1:20]
+sum(bbwo$BBWO)
 
 
 
@@ -110,5 +112,26 @@ bg <- bg[,-1]  # remove sp name column
 data_all <- cbind(bg,moch, mobl, lewo, bbwo)
 
 
+
+#################################################################
+###  Making different habitat types own variables in the data fram  ##########
+
+str(data_all)
+
+# remove observations on habitat types that are included less than 100 times
+tab <- count(data_all$landuse)
+tab[tab$freq<50,]
+x<-tab$x[tab$freq<50]
+dat <- data_all[!(data_all$landuse %in% list(x,0)),]
+str(dat)
+
+long <- acm.disjonctif(dat[3])  # make the landuse into long format 
+names(long) <- c("Evergreen_NF","Evergreen_BF", "Mixed_forest","Closed_shrub", 
+                 "Open_shrub", "Wood_sav","Sav", 
+                         "Grass", "Wetland", "Crop", "Urban", "Crop_vegetation", "Barren")
+combined <- cbind(dat, long)
+
+
 setwd("C:/Users/kvieka/Documents/GitHub/birdDist/data/birdDist")
-write.csv(data_all, file="Bird_presence_absence.csv", row.names = F)
+write.csv(combined, file="Bird_presence_absence.csv", row.names = F)
+
